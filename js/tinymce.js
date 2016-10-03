643134481a414f6788a03e0163f136b80152f820
Vitalii <vitalii.skorokhod@gmail.com>
2016-10-03 12:11:47 -0500

    Added width and height params to TinyMCE modal window

--- js/tinymce.js
+++ js/tinymce.js
@@ -1,20 +1,34 @@
 (function() {
-    tinymce.PluginManager.add('sitekit_tinymce_button', function( editor, url ) {
-        editor.addButton( 'sitekit_tinymce_button', {
+    tinymce.PluginManager.add('sitekit_tinymce_button', function(editor, url) {
+        editor.addButton('sitekit_tinymce_button', {
             text: 'Sitekit',
             type: 'menubutton',
             menu: [{
 				text: 'Iframe',
 				onclick: function() {
-					editor.windowManager.open( {
-						title: 'Insert iframe shortcode params',
-						body: [{
-							type: 'textbox',
-							name: 'src',
-							label: 'Source'
-						}],
-						onsubmit: function( e ) {
-							editor.insertContent( '[sitekit_iframe src="' + e.data.src + '"]');
+					editor.windowManager.open({
+						title: 'Iframe shortcode params',
+						body: [
+							{
+								type: 'textbox',
+								name: 'src',
+								label: 'src (source)'
+							},
+							{
+								type: 'textbox',
+								name: 'width',
+								label: 'width',
+								value: '100%'
+							},
+							{
+								type: 'textbox',
+								name: 'height',
+								label: 'height',
+								value: '500'
+							}
+						],
+						onsubmit: function(e) {
+							editor.insertContent('[sitekit_iframe src="' + e.data.src + '" width="' + e.data.width + '" height="' + e.data.height + '"]');
 						}
 					});
 				}
